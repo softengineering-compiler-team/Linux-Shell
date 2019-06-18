9202bf08aa52c57be3e09cc1633d4fbd9d4a9a6f
cs-Zeyu <shengzeyu19_98@163.com>
2019-06-18 15:02:48 +0800

    I/O redirection beta

--- src/Shell4Linux.cpp
+++ src/Shell4Linux.cpp
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include<errno.h>
+#include<pwd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 
@@ -9,9 +11,45 @@
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\r\n\a"
 
+#define BUF_SZ 256
+#define TRUE 1
+#define FALSE 0
+
+const char* COMMAND_EXIT = "exit";
+const char* COMMAND_HELP = "help";
+const char* COMMAND_CD = "cd";
+const char* COMMAND_IN = "<";
+const char* COMMAND_OUT = ">";
+const char* COMMAND_PIPE = "|";
+char **args;
+
+enum {
+	RESULT_NORMAL,
+	ERROR_FORK,
+	ERROR_COMMAND,
+	ERROR_WRONG_PARAMETER,
+	ERROR_MISS_PARAMETER,
+	ERROR_TOO_MANY_PARAMETER,
+	ERROR_CD,
+	ERROR_SYSTEM,
+	ERROR_EXIT,
+
+	/* 重定向的错误信息 */
+	ERROR_MANY_IN,
+	ERROR_MANY_OUT,
+	ERROR_FILE_NOT_EXIST,
+	
+	/* 管道的错误信息 */
+	ERROR_PIPE,
+	ERROR_PIPE_MISS_PARAMETER
+};
+
 int cd(char **args);
 int help(char **args);
 int exit(char **args);
+int callCommandWithPipe(int left, int right);
+int callCommandWithRedi(int left, int right);
+int isCommandExist(const char* command);
 
 char *builtin_str[] = {
   "cd",
@@ -53,28 +91,6 @@ int cd(char **args) {
   return 1;
 }
 
-int launch(char **args) {
-  	pid_t pid;
-  	int status;
-
- 	pid = fork();
-  	if (pid == 0) {
-    	// Child process
-    	if (execvp(args[0], args) == -1) {
-      		perror("Linux-Shell:");
-    	}
-    	exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-    	// Error forking
-    	perror("Linux-Shell:");
-  	} else {
-    	// Parent process
-    	do {
-      		waitpid(pid, &status, WUNTRACED);
-    	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  	}
-  	return 1;
-}
 
 int exit(char **args) {
   return 0;
@@ -113,7 +129,7 @@ char * read_line() {
   	}
 }
 
-char **split_line(char *line) {
+char **split_line(char *line,int &commandNum) {
   int bufsize = TOK_BUFSIZE, position = 0;
   char **tokens = (char **)malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
@@ -142,10 +158,89 @@ char **split_line(char *line) {
     token = strtok(NULL, TOK_DELIM);
   }
   tokens[position] = NULL;
+  commandNum=position;
   return tokens;
 }
 
-int execute(char **args) {
+int isCommandExist(const char* command) { // 判断指令是否存在
+	if (command == NULL || strlen(command) == 0) return FALSE;
+
+	int result = TRUE;
+	
+	int fds[2];
+	if (pipe(fds) == -1) {
+		result = FALSE;
+	} else {
+		/* 暂存输入输出重定向标志 */
+		int inFd = dup(STDIN_FILENO);
+		int outFd = dup(STDOUT_FILENO);
+
+		pid_t pid = vfork();
+		if (pid == -1) {
+			result = FALSE;
+		} else if (pid == 0) {
+			/* 将结果输出重定向到文件标识符 */
+			close(fds[0]);
+			dup2(fds[1], STDOUT_FILENO);
+			close(fds[1]);
+
+			char tmp[BUF_SZ];
+			sprintf(tmp, "command -v %s", command);
+			system(tmp);
+			exit(1);
+		} else {
+			waitpid(pid, NULL, 0);
+			/* 输入重定向 */
+			close(fds[1]);
+			dup2(fds[0], STDIN_FILENO);
+			close(fds[0]);
+
+			if (getchar() == EOF) { // 没有数据，意味着命令不存在
+				result = FALSE;
+			}
+			
+			/* 恢复输入、输出重定向 */
+			dup2(inFd, STDIN_FILENO);
+			dup2(outFd, STDOUT_FILENO);
+		}
+	}
+
+	return result;
+}
+
+int launch(char **args,int commandNum) {
+  	pid_t pid;
+  	int status;
+
+ 	pid = fork();
+  	if (pid == 0) {
+    	// Child process
+		/* 获取标准输入、输出的文件标识符 */
+		int inFds = dup(STDIN_FILENO);
+		int outFds = dup(STDOUT_FILENO);
+		int result = callCommandWithPipe(0, commandNum);
+		
+		/* 还原标准输入、输出重定向 */
+		dup2(inFds, STDIN_FILENO);
+		dup2(outFds, STDOUT_FILENO);
+		exit(result);
+    	// if (execvp(args[0], args) == -1) {
+      	// 	perror("Linux-Shell:");
+    	// }
+    	// exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+    	// Error forking
+    	perror("Linux-Shell:");
+  	} else {
+    	// Parent process
+    	do {
+      		waitpid(pid, &status, WUNTRACED);
+    	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  	}
+  	return 1;
+}
+
+int execute(char **args,int commandNum) {
   	int i;
 
   	if (args[0] == NULL) {
@@ -158,22 +253,181 @@ int execute(char **args) {
     	}
   	}
 
-  	return launch(args);
+  	return launch(args,commandNum);
 }
 
 void loop() {
 	char *line;
-  	char **args;
+
   	int status;
   	do {
-    	printf("> ");
+    	printf("bug>: ");
     	line = read_line();
-    	args = split_line(line);
-    	status = execute(args);
+		int tmp=0;
+		int &commandNum=tmp;
+    	args = split_line(line,commandNum);
+    	status = execute(args,commandNum);
     	free(line);
     	free(args);
   	} while (status);
 }
+
+int callCommand(int commandNum) { // 给用户使用的函数，用以执行用户输入的命令
+	pid_t pid = fork();
+	if (pid == -1) {
+		return ERROR_FORK;
+	} else if (pid == 0) {
+		/* 获取标准输入、输出的文件标识符 */
+		int inFds = dup(STDIN_FILENO);
+		int outFds = dup(STDOUT_FILENO);
+
+		int result = callCommandWithPipe(0, commandNum);
+		
+		/* 还原标准输入、输出重定向 */
+		dup2(inFds, STDIN_FILENO);
+		dup2(outFds, STDOUT_FILENO);
+		exit(result);
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+		return WEXITSTATUS(status);
+	}
+}
+
+int callCommandWithPipe(int left, int right) { // 所要执行的指令区间[left, right)，可能含有管道
+	if (left >= right) return RESULT_NORMAL;
+	/* 判断是否有管道命令 */
+	int pipeIdx = -1;
+	for (int i=left; i<right; ++i) {
+		if (strcmp(args[i], COMMAND_PIPE) == 0) {
+			pipeIdx = i;
+			break;
+		}
+	}
+	if (pipeIdx == -1) { // 不含有管道命令
+		return callCommandWithRedi(left, right);
+	} else if (pipeIdx+1 == right) { // 管道命令'|'后续没有指令，参数缺失
+		return ERROR_PIPE_MISS_PARAMETER;
+	}
+
+	/* 执行命令 */
+	int fds[2];
+	if (pipe(fds) == -1) {
+		return ERROR_PIPE;
+	}
+	int result = RESULT_NORMAL;
+	pid_t pid = vfork();
+	if (pid == -1) {
+		result = ERROR_FORK;
+	} else if (pid == 0) { // 子进程执行单个命令
+		close(fds[0]);
+		dup2(fds[1], STDOUT_FILENO); // 将标准输出重定向到fds[1]
+		close(fds[1]);
+		
+		result = callCommandWithRedi(left, pipeIdx);
+		exit(result);
+	} else { // 父进程递归执行后续命令
+		int status;
+		waitpid(pid, &status, 0);
+		int exitCode = WEXITSTATUS(status);
+		
+		if (exitCode != RESULT_NORMAL) { // 子进程的指令没有正常退出，打印错误信息
+			char info[4096] = {0};
+			char line[BUF_SZ];
+			close(fds[1]);
+			dup2(fds[0], STDIN_FILENO); // 将标准输入重定向到fds[0]
+			close(fds[0]);
+			while(fgets(line, BUF_SZ, stdin) != NULL) { // 读取子进程的错误信息
+				strcat(info, line);
+			}
+			printf("%s", info); // 打印错误信息
+			
+			result = exitCode;
+		} else if (pipeIdx+1 < right){
+			close(fds[1]);
+			dup2(fds[0], STDIN_FILENO); // 将标准输入重定向到fds[0]
+			close(fds[0]);
+			result = callCommandWithPipe(pipeIdx+1, right); // 递归执行后续指令
+		}
+	}
+
+	return result;
+}
+
+int callCommandWithRedi(int left, int right) { // 所要执行的指令区间[left, right)，不含管道，可能含有重定向
+	if (!isCommandExist(args[left])) { // 指令不存在
+		return ERROR_COMMAND;
+	}	
+
+	/* 判断是否有重定向 */
+	int inNum = 0, outNum = 0;
+	char *inFile = NULL, *outFile = NULL;
+	int endIdx = right; // 指令在重定向前的终止下标
+
+	for (int i=left; i<right; ++i) {
+		if (strcmp(args[i], COMMAND_IN) == 0) { // 输入重定向
+			++inNum;
+			if (i+1 < right)
+				inFile = args[i+1];
+			else return ERROR_MISS_PARAMETER; // 重定向符号后缺少文件名
+
+			if (endIdx == right) endIdx = i;
+		} else if (strcmp(args[i], COMMAND_OUT) == 0) { // 输出重定向
+			++outNum;
+			if (i+1 < right)
+				outFile = args[i+1];
+			else return ERROR_MISS_PARAMETER; // 重定向符号后缺少文件名
+				
+			if (endIdx == right) endIdx = i;
+		}
+	}
+	/* 处理重定向 */
+	if (inNum == 1) {
+		FILE* fp = fopen(inFile, "r");
+		if (fp == NULL) // 输入重定向文件不存在
+			return ERROR_FILE_NOT_EXIST;
+		
+		fclose(fp);
+	}
+	
+	if (inNum > 1) { // 输入重定向符超过一个
+		return ERROR_MANY_IN;
+	} else if (outNum > 1) { // 输出重定向符超过一个
+		return ERROR_MANY_OUT;
+	}
+
+	int result = RESULT_NORMAL;
+	pid_t pid = vfork();
+	if (pid == -1) {
+		result = ERROR_FORK;
+	} else if (pid == 0) {
+		/* 输入输出重定向 */
+		if (inNum == 1)
+			freopen(inFile, "r", stdin);
+		if (outNum == 1)
+			freopen(outFile, "w", stdout);
+
+		/* 执行命令 */
+		char* comm[BUF_SZ];
+		for (int i=left; i<endIdx; ++i)
+			comm[i] = args[i];
+		comm[endIdx] = NULL;
+		execvp(comm[left], comm+left);
+		exit(errno); // 执行出错，返回errno
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+		int err = WEXITSTATUS(status); // 读取子进程的返回码
+
+		if (err) { // 返回码不为0，意味着子进程执行出错，用红色字体打印出错信息
+			printf("\e[31;1mError: %s\n\e[0m", strerror(err));
+		}
+	}
+
+
+	return result;
+}
+
 
 int main() {
 	loop();
